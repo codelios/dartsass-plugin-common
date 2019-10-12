c731e6a4fa4640091643756700e8702c9e81cb90
Karthik K <karthiknetworklink+code@gmail.com>
2019-10-12 20:12:28 +0530

    test: unit test cases

---
+++ __tests__/dartsasscompiler.spec.ts
@@ -0,0 +1,62 @@
+// Copyright (c) 2019 MalvaHQ
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+'use strict';
+import { expect } from 'chai';
+import 'mocha';
+import { DartSassCompiler } from '../src/dartsasscompiler';
+import { IDocument } from '../src/document';
+import { CompilerConfig } from '../src/config';
+import { getSassDocument} from './document';
+import { getNullLog, getBufLog } from './log';
+
+describe('DartsassCompiler SayVersion' , () => {
+
+    it('sayVersion', () => {
+        const compiler = new DartSassCompiler();
+        const config = new CompilerConfig();
+        config.sassBinPath = "/usr/local/bin/sass";
+        const _log = getBufLog();
+        compiler.sayVersion(config, _log).then(
+            function(data: any) {
+                // This value comes from the version installed using Dockerfile. Hence hardcoded. YMMV locally.
+                expect(data).to.equal('dart-sass\t1.23.0\t(Sass Compiler)\t[Dart]\ndart2js\t2.5.1\t(Dart Compiler)\t[Dart]');
+            },
+            function(err: any) {
+                expect(err).to.be.not.null;
+            }
+        );
+    });
+});
+
+describe('DartsassCompiler CompileDocument' , () => {
+
+    it('compileDocument', () => {
+        const compiler = new DartSassCompiler();
+        const document: IDocument = getSassDocument("/tmp", "/tmp/abc.scss", "abc");
+        const config = new CompilerConfig();
+        const _log = getNullLog();
+        compiler.compileDocument(document, config, _log).then(
+            result => {
+                expect(result).to.equal('Hello');
+            },
+            err => {
+                expect(err).to.be.null;
+            }
+        )
+    });
+});
+
+describe('DartsassCompiler Which' , () => {
+
+    it('which', () => {
+        const compiler = new DartSassCompiler();
+        const config = new CompilerConfig();
+        config.sassBinPath = "/usr/local/bin/sass";
+        const _log = getNullLog();
+        const result = compiler.which(config, _log);
+        expect(result).to.equal(config.sassBinPath);
+    });
+});
