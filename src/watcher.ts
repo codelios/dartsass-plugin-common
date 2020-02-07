c56162af3edddfa62aec9330f3e6de9cfd51de23
Karthik K <karthiknetworklink@gmail.com>
2020-02-07 14:30:36 +0530

    fix: crazy bug fix

--- src/watcher.ts
+++ src/watcher.ts
@@ -152,7 +152,7 @@ export function watchDirectory(srcdir: string, config: CompilerConfig) : Promise
     return  new Promise<string>(function(resolve, reject) {
         for(const watchDir of config.watchDirectories) {
             if (watchDir === srcdir) {
-                reject(`${watchDir} already being watched`);
+                // reject(`${watchDir} already being watched`);
                 return;
             }
         }
