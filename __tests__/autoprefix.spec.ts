003a02751106c15c98706514b2be78fc2c6866b0
Karthik K <karthiknetworklink@gmail.com>
2020-02-13 21:47:50 +0530

    fix: add tests

--- __tests__/autoprefix.spec.ts
+++ __tests__/autoprefix.spec.ts
@@ -5,10 +5,37 @@
 
 'use strict';
 import 'mocha';
-import { getVersions } from '../src/autoprefix';
-// import { expect } from 'chai';
+import { CompilerConfig } from '../src/config';
+import { CSSFile } from '../src/cssfile';
+import { getVersions, doAutoprefixCSS } from '../src/autoprefix';
+import { expect } from 'chai';
+
+const InputCSS = `
+.example {
+    display: grid;
+    transition: all .5s;
+    user-select: none;
+    background: linear-gradient(to bottom, white, black);
+}
+`;
 
 describe('autoprefix' , () => {
+
+    it('doAutoprefixCSS', (done) => {
+        const config = new CompilerConfig();
+        doAutoprefixCSS({
+            css: Buffer.from(InputCSS),
+            sourceMap: null,
+        }, config).then(
+            (value: CSSFile) => {
+                expect(value.css).to.be.not.null;
+                expect(value.sourceMap).to.be.not.null;
+            },
+            err => {
+                expect(err).to.be.not.null;
+            }
+        ).finally(done);
+    });
 
     it('getVersions', () => {
         const versions = getVersions();
